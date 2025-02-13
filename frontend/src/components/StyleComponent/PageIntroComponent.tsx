0cc0a5243ca490498c40e0f2a69ba06589b40f8f
Reward Steve <rewardstephen30@gmail.com>
2025-02-13 11:32:50 +0100

    update

--- frontend/src/components/StyleComponent/PageIntroComponent.tsx
+++ frontend/src/components/StyleComponent/PageIntroComponent.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import styles from "./../../styles/styledComponent.module.css"; // Import the CSS module
-import { PageIntroProps } from "../../router/mainRoutes";
-import SideComponent from "./SideComponent";
 
-const PageIntro: React.FC<PageIntroProps> = React.memo(({ children }) => {
+import SideComponent from "./SideComponent";
+import MainComponent from "./MainComponent";
+const PageIntro: React.FC = () => {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>{children}</main>
-
-      <SideComponent />
+      <main className={styles.mainHolder}>
+        <MainComponent>
+          <h1>Welcome to Our Website</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac urna
+            ipsum. Donec quis lectus vel neque efficitur bibendum.
+          </p>
+        </MainComponent>
+      </main>
+      <aside>
+        <SideComponent>
+          <h2>Sidebar</h2>
+          <p>Some content goes here</p>
+        </SideComponent>
+      </aside>
     </div>
   );
-});
+};
 export default PageIntro;
--- frontend/src/components/StyleComponent/SideComponent.tsx
+++ frontend/src/components/StyleComponent/SideComponent.tsx
@@ -1,16 +1,10 @@
 import * as React from "react";
-import style from "../../styles/styledComponent.module.css"; // Import the CSS module for styling
+import style from "../../styles/styledComponent.module.css";
 
-interface SideComponentProps {
-  children: React.ReactNode; // Content can be any React component or text
-}
-
-const SideComponent: React.FC<SideComponentProps> = ({ children }) => {
+export default function SideComponent({ children }: React.PropsWithChildren) {
   return (
     <>
       <main className={style.background}>{children}</main>
     </>
   );
-};
-
-export default React.memo(SideComponent);
+}
