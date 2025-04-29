d5fdbeed7110a41ac8191ac91dfa9b55367eb049
Reward Steve <rewardstephen30@gmail.com>
2025-04-29 01:35:06 +0100

    modified:user-authentication

--- backend/src/controllers/User.js
+++ backend/src/controllers/User.js
@@ -1,31 +1,16 @@
 const User = require("../models/UserModels/User");
-const Admin = require("../models/UserModels/Admin");
-const Doctor = require("../models/UserModels/Doctor");
-const LabTechnician = require("../models/UserModels/LabTechnician");
-const Pharmacist = require("../models/UserModels/Pharmacist");
-const Nurse = require("../models/UserModels/Nurse");
-const Patient = require("../models/UserModels/Patient");
 
 const {
   catchAsync,
   handleNotFound,
   handleNoResult,
-  findAndUpdate,
 } = require("../Utils/reusableFunctions");
 
 //GET USER BY ID
 const getUserById = catchAsync(async (req, res, next) => {
-  const { userId } = req.params.id;
+  const { id } = req.params;
 
-  const user =
-    (await User.findById({ userId })) ||
-    (await Admin.findById({ userId })) ||
-    (await Doctor.findById({ userId })) ||
-    (await LabTechnician.findById({ userId })) ||
-    (await Pharmacist.findById({ userId })) ||
-    (await Nurse.findById({ userId })) ||
-    (await Patient.findById({ userId }));
-
+  const user = await User.findById({ id });
   handleNoResult(user, "No users found", next);
 
   res.status(200).json({
@@ -37,15 +22,11 @@ const getUserById = catchAsync(async (req, res, next) => {
 
 //UPDATE USER BY ID
 const updateUserById = catchAsync(async (req, res, next) => {
-  const { id } = req.params.id;
-  const user =
-    (await findAndUpdate(User, id, req.body)) ||
-    (await findAndUpdate(Admin, id, req.body)) ||
-    (await findAndUpdate(Doctor, id, req.body)) ||
-    (await findAndUpdate(LabTechnician, id, req.body)) ||
-    (await findAndUpdate(Pharmacist, id, req.body)) ||
-    (await findAndUpdate(Nurse, id, req.body)) ||
-    (await findAndUpdate(Patient, id, req.body));
+  const { id } = req.params;
+  const user = await User.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: false,
+  });
 
   handleNotFound(user, `No user with ID ${req.params.id} found`, next);
 
@@ -57,15 +38,8 @@ const updateUserById = catchAsync(async (req, res, next) => {
 
 //DELETE USER BY ID
 const deleteUserById = catchAsync(async (req, res, next) => {
-  const { id } = req.params.id;
-  const user =
-    (await User.findByIdAndDelete(id)) ||
-    (await Admin.findByIdAndDelete(id)) ||
-    (await Doctor.findByIdAndDelete(id)) ||
-    (await LabTechnician.findByIdAndDelete(id)) ||
-    (await Pharmacist.findByIdAndDelete(id)) ||
-    (await Nurse.findByIdAndDelete(id)) ||
-    (await Patient.findByIdAndDelete(id));
+  const { id } = req.params;
+  const user = await User.findByIdAndDelete(id);
 
   handleNotFound(user, `No user with ID ${id} found`, next);
 
